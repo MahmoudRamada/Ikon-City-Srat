c3798cc4f3cd52b61af1b8dea69656c35359ade1
mhd9111 <mohamed.moftah@gmail.com>
2015-03-12 21:42:43 +0000

    commit create getUserByEmail and its tests

--- src/main/java/com/ikonsoft/services/UserService.java
+++ src/main/java/com/ikonsoft/services/UserService.java
@@ -1,27 +1,59 @@
 
 package com.ikonsoft.services;
 
+import java.util.List;
+
+import com.ikonsoft.model.Question;
 import com.ikonsoft.model.User;
 import com.ikonsoft.utils.HibernateUtil;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
  
 public class UserService {
-    public void createUser(User user)
-    {  int id;
+	
+    public int createUser(User user)
+    {  int id = 0;
+    User existingUser = getUserByEmailId(user.getEmailId());
+    if (existingUser!=null){ 
+    System.out.println("Cant create user , email already exists");
+    return 0;
+    }
    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-      Transaction trns = null;  
+    
     try {
         session.beginTransaction();
-    
         session.save(user);
         session.getTransaction().commit();
           id = user.getUserId();
-        System.out.println("\n\nPerson Id :"+id);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }  
-                     	          
-     
+        System.out.println("\n\nNew Person Id :"+id);
+        } catch (RuntimeException e) {  e.printStackTrace(); } 
+    
+          return id;           	          
     }
+    
+    public User getUserByEmailId(String email)
+    { User user = null;
+    	 Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    	   session.beginTransaction();
+    	     String hql = "SELECT u FROM User u WHERE u.emailId = :email";
+    	     Query query = session.createQuery(hql);
+    	     query.setParameter("email",email); 
+    	     
+    	     List results = query.list();
+    	if (results.size()>0) {      user = (User) results.get(0);
+    	
+    	   System.out.println("User i"+user.getUserId());
+    	}else {
+    		  System.out.println("User Not found");
+    	}
+    	  session.getTransaction().commit();
+    	   
+    	    return (user);        	          
+    }
+  
+    
+    
+    
 }
--- src/test/java/com/ikonsoft/services/UserServiceTest.java
+++ src/test/java/com/ikonsoft/services/UserServiceTest.java
@@ -6,11 +6,13 @@
 package com.ikonsoft.services;
 
 import com.ikonsoft.model.User;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -40,14 +42,25 @@ public class UserServiceTest {
 
     @Test
     public void testCreateUser() {
-        System.out.println("createUser");
+        System.out.println("create User Test");
         UserService userService = new UserService();
         User user=new User();
-        user.setEmailId("moh3299212@gmail.com");
+        user.setEmailId("moh3292@gmail.com");
         user.setPassword("123");
-         userService.createUser(user);
-        System.out.println("The user id created ");
+       System.out.println("The user id returns "+userService.createUser(user) )  ;
+       
          
     }
     
+	@Test
+	public final void testGetUserByEmailId() {
+		 System.out.println("Test Get User by Email");
+	        UserService userService = new UserService();
+	        User user;
+	        String email="moh1@gmail.com";
+	       
+	       user=  userService.getUserByEmailId(email);
+	         
+	        System.out.println("The user is found: "+user.getFirstName());
+	}
 }
